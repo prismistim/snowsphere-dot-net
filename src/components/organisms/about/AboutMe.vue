a4d577a3d7dd293343142a0248963e5c333006fe
snowsphere <prismistim@hotmail.com>
2021-11-08 18:02:42 +0000

    change: プロフィール画像と基本情報の並びを縦に変更

--- src/components/organisms/about/AboutMe.vue
+++ src/components/organisms/about/AboutMe.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="md:flex md:mb-8">
-    <div class="md:inline-block md:w-[30vw] lg:w-[25vw] 2xl:w-[23vw] md:mr-7 mb-4 md:mb-0">
+  <div class="text-center">
+    <div class="md:w-[30vw] lg:w-[25vw] 2xl:w-[23vw] mb-4 md:mb-0 mx-auto">
       <rounded-image src="/me.jpg" />
     </div>
-    <div class="md:inline-block">
-      <card>
-        <template #description>
-          <div class="mb-8">
-            <profile />
-          </div>
-        </template>
-      </card>
-    </div>
+    <card class="block">
+      <template #description>
+        <div class="mb-8">
+          <profile />
+        </div>
+      </template>
+    </card>
   </div>
-  <div class="row-span-1 md:col-span-5 lg:col-span-2 xl:col-span-4 mb-12">
+  <div class="mb-12">
     <div class="md:border-b md:mb-6 border-gray-100" />
     教育考えからあれはその十年に駄目に担任するたのないんた。ただ多年のように人人の片仮名をなりつけよば手自分で云えているようんは必ずしも秋刀魚家来なしのなと、なぜ人方現われないまで若いという行かたなけれ外国を不審にそれの事実に寄っていらっしゃるませ、国家はむしろ非常ですで、便所もたといするましですと買うて、機械私の天下という、何を乱暴知らものが私の一番の形の変っでと申すうものあり。
   </div>
