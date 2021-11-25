bae2c2bcbfe821efefbae3557bfe733dd3b57dba
snowsphere <info@snowsphere.net>
2021-11-25 18:34:51 +0000

    feat: WeeklyChartの追加

---
+++ src/components/molecules/about/MusicListItem.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  rank: {
+    type: Number,
+    default: 1
+  },
+  thumb: {
+    type: String,
+    default: ''
+  },
+  title: {
+    type: String,
+    default: ''
+  },
+  artist: {
+    type: String,
+    default: ''
+  },
+  playCount: {
+    type: Number,
+    default: 0
+  }
+})
+</script>
+
+<template>
+  <li class="grid grid-cols-8 text-md py-4 md:py-4 gap-y-3">
+    <div>
+      <div class="font-semibold">{{ props.rank }}</div>
+      <div class="text-gray-300">({{ props.playCount }})</div>
+    </div>
+    <div class="col-span-7">
+      <div class="mb-1 text-indigo-400 text-sm">{{ props.artist }}</div>
+      <div class="">{{ props.title }}</div>
+    </div>
+  </li>
+  <div v-if="props.rank !== 10" class="border-b border-indigo-100" />
+</template>
---
+++ src/components/organisms/about/MyWeeklyMusicChartList.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import axios, { AxiosResponse } from 'axios'
+import { ref, computed } from 'vue'
+import { Response, Track } from '/@/types/LastfmWeeklyChart'
+import SectionCard from '/@/components/molecules/SectionCard.vue'
+import SubTitle from '/@/components/atoms/SubTitle.vue'
+import MusicListItem from '/@/components/molecules/about/MusicListItem.vue';
+
+const chartsRawData = ref<Track[]>([])
+
+const fetchMusicChart = async () => {
+  const apiKey = import.meta.env.VITE_LF_API_KEY
+
+  try {
+    const res: AxiosResponse<Response> = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=user.getweeklytrackchart&user=prismist-m&api_key=${apiKey}&format=json`)
+
+    if (res.status === 200) {
+      chartsRawData.value = res.data.weeklytrackchart.track
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const MusicCharts = computed(() => {
+  if (chartsRawData.value.length === 0) {
+    return []
+  }
+
+  const target = chartsRawData.value.map((item: Track) => {
+    return {
+      rank: Number(item['@attr'].rank),
+      thumb: item.image[1]["#text"],
+      title: item.name,
+      artist: item.artist["#text"],
+      playCount: Number(item.playcount)
+    }
+  })
+
+  return target.slice(0, 10)
+})
+
+fetchMusicChart()
+</script>
+
+<template>
+  <section-card>
+    <template #title>
+      <sub-title> Weekly Music Chart </sub-title>
+    </template>
+    <template #content>
+      <ul>
+        <music-list-item
+          v-for="item of MusicCharts"
+          :key="item.title"
+          :rank="item.rank"
+          :thumb="item.thumb"
+          :title="item.title"
+          :artist="item.artist"
+          :play-count="item.playCount"
+        ></music-list-item>
+      </ul>
+    </template>
+  </section-card>
+</template>
