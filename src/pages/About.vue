e69474d2716768f629d3b109b6b6e12ab4699bf1
snowsphere <info@snowsphere.net>
2023-05-04 23:12:06 +0900

    change: 不要なコンポーネントとその記述の削除

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import Contents from '/@/components/templates/Contents.vue'
-import AboutMe from '/@/components/organisms/about/AboutMe.vue'
 import BasicInfoCard from '/@/components/about/BasicInfoCard.vue'
 import SectionTitle from '/@/components/atoms/SectionTitle.vue'
 import CareerList from '/@/components/organisms/about/CareerList.vue'
 import SideBySide from '/@/components/templates/SideBySide.vue'
 import SkilList from '/@/components/organisms/about/SkilList.vue'
 import MyWeeklyMusicChartList from '/@/components/organisms/about/MyWeeklyMusicChartList.vue'
-import AboutMeNew from '/@/components/organisms/about/AboutMeNew.vue'
 </script>
 
 <template>
