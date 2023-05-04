9b85da4250f1f4002974e2a3cadca3499aaf7689
snowsphere <info@snowsphere.net>
2023-05-04 18:30:52 +0900

    fix: サイズ感を修正 (WIP)

--- src/components/organisms/about/AboutMeNew.vue
+++ src/components/organisms/about/AboutMeNew.vue
@@ -3,26 +3,26 @@
     <div class="bg-cover bg-center lg:h-auto h-80" style="background-image: url('/me.jpg')"></div>
     <div>
       <div class="border">
-        <div class="p-8 border">
-          <div class="text-neutral-400 font-ubuntu font-semibold text-2xl">Name</div>
-          <div class="mt-2">
-            <span class="text-5xl">村重 海月</span>
+        <div class="px-6 py-4 border">
+          <div class="text-neutral-400 font-ubuntu font-semibold text-lg">Name</div>
+          <div class="mt-1">
+            <span class="text-3xl">村重 海月</span>
           </div>
-          <div class="mt-2">
+          <div class="mt-1">
             <span class="text-xl font-ubuntu-mono font-medium">Mizuki Murashige</span>
           </div>
         </div>
-        <div class="p-8 border">
-          <div class="text-neutral-400 font-ubuntu font-semibold text-xl">a.k.a.</div>
-          <div class="text-2xl mt-1 font-ubuntu-mono">snowsphere</div>
+        <div class="px-6 py-4 border">
+          <div class="text-neutral-400 font-ubuntu font-semibold text-lg">a.k.a.</div>
+          <div class="text-xl mt-1 font-ubuntu-mono">snowsphere</div>
         </div>
-        <div class="p-8 border">
-          <div class="text-neutral-400 font-ubuntu font-semibold text-xl">Birth</div>
-          <div class="text-2xl mt-1 font-ubuntu-mono">1998.08.08</div>
+        <div class="px-6 py-4 border">
+          <div class="text-neutral-400 font-ubuntu font-semibold text-lg">Birth</div>
+          <div class="text-xl mt-1 font-ubuntu-mono">1998.08.08</div>
         </div>
-        <div class="p-8 border h-full">
-          <div class="text-neutral-400 font-ubuntu font-semibold text-xl">Place</div>
-          <div class="text-2xl mt-1 font-ubuntu-mono">Gifu, Japan</div>
+        <div class="px-6 py-4 border h-full">
+          <div class="text-neutral-400 font-ubuntu font-semibold text-lg">Place</div>
+          <div class="text-xl mt-1 font-ubuntu-mono">Gifu, Japan</div>
         </div>
       </div>
     </div>
