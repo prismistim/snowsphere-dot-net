daa2d9312de6d986342bce819c1f67de596fae44
snowsphere <info@snowsphere.net>
2023-06-16 02:37:51 +0900

    feat: 投稿詳細のスタイルを追加

--- src/pages/WorkDetail.vue
+++ src/pages/WorkDetail.vue
@@ -22,19 +22,31 @@ onMounted(async () => {
   }
 
   h1 {
-    @apply mb-4 py-4 text-3xl font-semibold border-b dark:text-neutral-300;
+    @apply mb-4 py-6 text-2xl font-semibold border-b border-dashed border-neutral-500 dark:text-neutral-300;
   }
 
   h2 {
-    @apply mb-2 py-2 text-2xl font-semibold border-b dark:text-neutral-300;
+    @apply mt-6 mb-2 py-2 text-xl font-semibold border-b border-neutral-500 dark:text-neutral-300;
   }
 
   h3 {
-    @apply mb-2 text-xl font-semibold dark:text-neutral-300;
+    @apply mb-2 text-lg font-semibold dark:text-neutral-300;
   }
 
   p {
-    @apply py-2 mb-2 dark:text-neutral-300;
+    @apply mt-4 dark:text-neutral-300;
+  }
+
+  p + p {
+    @apply mt-2;
+  }
+
+  ul {
+    @apply list-disc ml-4 mb-3;
+  }
+
+  li > ul {
+    @apply mb-0;
   }
   
   img {
